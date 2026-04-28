60240d7b2f92492bfd7cb25871111ba2471a1b15
Malte Laukötter <malte.laukoetter@digitalservice.bund.de>
2026-04-28 09:54:07 +0200

    Webpdf and Princexml test
    RISDEV-0000

--- example/tailwind-css/postcss.config.ts
+++ example/tailwind-css/postcss.config.ts
@@ -1,5 +1,8 @@
 export default {
   plugins: {
+    'postcss-nesting': {},
+    '@csstools/postcss-cascade-layers': {},
+    'postcss-logical': {},
     "@tailwindcss/postcss": {
       optimize: false
     },
